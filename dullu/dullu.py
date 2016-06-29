9dce5b2c8e5d3b3da16df6fb018181901330d291
IntrepidBrit <mark@jonesgroup.co.uk>
2016-06-29 15:38:52 +0100

    Migrated the link rot code from `nidaba` to here.

--- dullu/dullu.py
+++ dullu/dullu.py
@@ -1,1 +1,56 @@
 # -*- coding: utf-8 -*-
+import http.client
+import socket
+from urllib.parse import urlparse
+
+from bs4 import BeautifulSoup
+from dullu.utils.HTTPStatus import HTTPStatus
+
+
+def get_all_links(s):
+    """
+    Find all the urls in the text and stick them in a list for later processing.
+
+    :param s: Input string
+    :return: List of links
+    """
+    soup = BeautifulSoup(s, "html.parser")
+    return [link.get('href') for link in soup.find_all('a')]
+
+
+def get_link_status_code(link):
+    """
+
+    :param link: a string containing an url
+    :return:
+    """
+    # noinspection PyArgumentList
+    try:
+        parsed_link = urlparse(link)
+        conn = http.client.HTTPConnection(parsed_link.netloc)
+        conn.request("HEAD", parsed_link.path)
+        return HTTPStatus(conn.getresponse().status,)
+    except http.client.BadStatusLine:
+        return HTTPStatus.BAD_REQUEST
+    except socket.gaierror as e:
+        if e.errno == 11001:
+            # Technically speaking, a HTTP status code doesn't make sense because DNS lookup has failed ...
+            return HTTPStatus.NOT_FOUND
+        else:
+            raise
+
+
+def get_all_link_status_codes(list_of_links):
+    """
+    Takes a list of links, tries to establish a connection and gets the status code. Lumps both together in a dict and
+    then appends that dict to a list.
+
+    :param list_of_links: list of link url strings
+    :return: list of link dicts with keys: link, code
+    """
+
+    links = []
+    for link in list_of_links:
+        links.append({'link': link, 'code': get_link_status_code(link)})
+
+    return links
--- tests/test_dullu.py
+++ tests/test_dullu.py
@@ -1,36 +1,61 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import json
 
-"""
-test_dullu
-----------------------------------
-
-Tests for `dullu` module.
-"""
-
-from click.testing import CliRunner
-
-from dullu import cli
+import os
+import random
+from dullu import dullu
+from dullu.utils.HTTPStatus import HTTPStatus
 
 
-class TestDullu(object):
+def test_get_all_urls():
+    """
+    The function responsible for grabbing all the links from a block of text, and putting them into a list of links
+    :return: None
+    """
+    assert dullu.get_all_links("") == []
 
-    @classmethod
-    def setup_class(cls):
-        pass
+    assert dullu.get_all_links("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales faucibus
+    elementum. Mauris rutrum interdum turpis, id convallis nibh porttitor et. Pellentesque ultrices tortor ut leo
+    fermentum sollicitudin. Donec urna ipsum, blandit sit amet posuere quis, suscipit a orci. Mauris vel ex et tortor
+    lacinia tincidunt at id turpis. Pellentesque dapibus, risus eu aliquet ornare, nisl lorem porttitor eros, vel auctor
+    nisl ante vestibulum ipsum. Ut iaculis arcu quis interdum facilisis. Pellentesque non mollis ante, vitae semper sapien.
+    Proin malesuada nisl at nulla sodales ultrices. Sed eget fermentum velit. Cras sed turpis pharetra, consectetur
+    mauris non, sollicitudin elit. Etiam non magna lacus. Mauris gravida urna sit amet rutrum malesuada. Nulla ultricies
+    eros vel nunc ornare maximus. Duis vitae arcu sit amet quam varius accumsan et ac dui. Morbi varius volutpat nulla,
+    non venenatis libero dictum vel. Donec quis massa libero. Proin vitae efficitur est, nec posuere ligula. Nullam quis
+    ligula.""") == []
 
-    def test_something(self):
-        pass
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'dullu.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    assert dullu.get_all_links("""<html><head><title>Hello World!</title></head><body><h1>Hello World!</h1><p>I like
+                                turtles</p></body></html>""") == []
 
-    @classmethod
-    def teardown_class(cls):
-        pass
+    assert dullu.get_all_links("<a href=\"http://stackoverflow.com/a/1732454/1241495\">issit zalgo?</a>") != \
+           ["http://stackoverflow.com/a/2504454/673991"]
+
+    assert dullu.get_all_links("<a href=\"http://stackoverflow.com/a/1732454/1241495\">oh, noes zalgo!</a>") == \
+           ["http://stackoverflow.com/a/1732454/1241495"]
+
+    f = open(os.path.join(os.path.dirname(__file__), 'data', 'test_question.json'), 'r')
+    try:
+        assert dullu.get_all_links(json.load(f)['Body']) == \
+               ['http://c-faq.com/stdio/scanfprobs.html',
+                'http://c-faq.com/stdio/getsvsfgets.html',
+                'http://stackoverflow.com/questions/9378500/why-is-splitting-a-string-slower-in-c-than-python']
+    finally:
+        f.close()
+
+
+def test_get_link_status_code():
+    """
+    The function responsible for trying to grab the status code of the link
+    :return: None
+    """
+
+    assert dullu.get_link_status_code("") == HTTPStatus.BAD_REQUEST
+    assert dullu.get_link_status_code("sopython.com") == HTTPStatus.BAD_REQUEST
+    assert dullu.get_link_status_code("http://sopython.com") == HTTPStatus.OK
+    assert dullu.get_link_status_code("http://ww.thiswebsiteshouldneverexistyo.roflbob") == HTTPStatus.NOT_FOUND
+
+    # Can take a long time.
+    for i in range(0,3):
+        test_status = random.choice(list(HTTPStatus))
+        assert dullu.get_link_status_code("http://httpstat.us/{code}".format(code=test_status.value)) == test_status
 
